0abd0028bdb20d8f54402562fc8e9039906737ea
baranwang <me@baran.wang>
2022-01-31 10:13:17 +0800

    feat: dlercloud

---
+++ Modules/DlerCloud/DlerCloud.js
@@ -0,0 +1,70 @@
+async function request(url, params) {
+  const body = JSON.stringify(params);
+  return new Promise((resolve, reject) => {
+    $httpClient.post(
+      {
+        url: `https://dler.cloud${url}`,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body,
+      },
+      (error, response, res) => {
+        if (error) {
+          reject(error);
+        } else {
+          const { ret, msg, data } = JSON.parse(res);
+          if (ret === 200) {
+            resolve(data);
+          } else {
+            reject(msg);
+          }
+        }
+      }
+    );
+  });
+}
+
+const API = {
+  Login: async function (email, passwd) {
+    return await request('/api/v1/login', {
+      email,
+      passwd,
+    });
+  },
+  Logout: async function (access_token) {
+    return await request('/api/v1/logout', { access_token });
+  },
+  Checkin: async function (access_token, multiple = 50) {
+    return await request('/api/v1/checkin', {
+      access_token,
+      multiple,
+    });
+  },
+};
+
+function getOptions() {
+  const options = {};
+  if (typeof $argument != 'undefined') {
+    try {
+      const params = Object.fromEntries(
+        $argument
+          .split('&')
+          .map((item) => item.split('='))
+          .map(([k, v]) => [k, decodeURIComponent(v)])
+      );
+      Object.assign(options, params);
+    } catch (error) {
+      console.error(`$argument 解析失败，$argument: + ${argument}`);
+    }
+  }
+
+  return options;
+}
+
+(async () => {
+  const { email, password, multiple } = getOptions();
+  const user = await API.Login(email, password);
+  const log = await API.Checkin(user.token, multiple);
+  $done(log.checkin);
+})();
